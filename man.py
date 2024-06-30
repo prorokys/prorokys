07f83cd77bdac19a5182f58632634df0fc18940c
prorokys <159473667+prorokys@users.noreply.github.com>
2024-06-30 11:04:04 +0200

    Update man.py

--- man.py
+++ man.py
@@ -7,23 +7,23 @@ from langchain.docstore.document import Document
 import uvicorn
 
 
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+app = FastAPI()  # Initialize FastAPI
+templates = Jinja2Templates(directory="templates")  # Initialize Jinja2Templates
 
 
 @app.post("/summarize", response_class=HTMLResponse)
 def summarize(request: Request, text: str = Form(...)):
-
+    # Load model from HuggingFace
     model = HuggingFacePipeline.from_model_id(
         model_id="sshleifer/distilbart-cnn-12-6",
         task="summarization",
         model_kwargs={"max_length": 1000},
     )
 
-    data = [Document(page_content=text)]
-    chain = load_summarize_chain(model)
+    data = [Document(page_content=text)]  # Load text to summarize
+    chain = load_summarize_chain(model)  # Load summarize chain from model
 
-    summary = chain.run(data)
+    summary = chain.run(data)  # Run summarize chain on text to get summary
 
     return templates.TemplateResponse(
         "index.html",
@@ -32,4 +32,4 @@ def summarize(request: Request, text: str = Form(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+    uvicorn.run(app, host="127.0.0.1", port=8001)  # Run uvicorn server
